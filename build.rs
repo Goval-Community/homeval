0af0469d34c3c558cee977704e375871407d4350
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2023-05-18 19:30:34 -0700

    Tidy up `build.rs`

--- build.rs
+++ build.rs
@@ -1,29 +1,26 @@
-// use deno_core::{include_js_files, Extension};
-// use std::env;
-// use std::path::PathBuf;
 use std::process::Command;
 extern crate prost_build;
 
 fn main() {
+    // Only rerun if a protobuf changed, or if api.js changed
     println!("cargo:rerun-if-changed=src/protobufs,api.js");
 
+    // Compile protobufs
     prost_build::compile_protos(
         &["src/protobufs/goval.proto", "src/protobufs/client.proto"],
         &["src/"],
     )
     .unwrap();
 
+    // Run: bun install
     let output = Command::new("bun")
         .arg("install")
         .output()
-        .expect("Failed to install js packages");
+        .expect("Getting bun output failed");
 
-    assert!(
-        output.status.code().expect("exit code needed") == 0,
-        "Bun install failed"
-    );
+    assert!(output.status.success(), "Running bun install failed");
 
-    // bun x esbuild ./api.js --bundle --minify --platform=browser --outfile=src/api.js
+    // Run: bun x esbuild ./api.js --bundle --minify --platform=browser --outfile=src/api.js
     let output = Command::new("bun")
         .arg("x")
         .arg("esbuild")
@@ -33,28 +30,7 @@ fn main() {
         .arg("--platform=browser")
         .arg("--outfile=src/api.js")
         .output()
-        .expect("Esbuild failed");
+        .expect("Getting esbuild output failed");
 
-    assert!(
-        output.status.code().expect("exit code needed") == 0,
-        "Esbuild failed"
-    );
-
-    // let runjs_extension = Extension::builder("runjs")
-    //     .esm(include_js_files!("src/runtime.js",))
-    //     .build();
-
-    // // Build the file path to the snapshot.
-    // let o = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    // let snapshot_path = o.join("RUNTIME_SNAPSHOT.bin");
-
-    // // Create the snapshot.
-    // deno_core::snapshot_util::create_snapshot(deno_core::snapshot_util::CreateSnapshotOptions {
-    //     cargo_manifest_dir: env!("CARGO_MANIFEST_DIR"),
-    //     snapshot_path: snapshot_path,
-    //     startup_snapshot: None,
-    //     extensions: vec![runjs_extension],
-    //     compression_cb: None,
-    //     snapshot_module_load_cb: None,
-    // })
+    assert!(output.status.success(), "Running esbuild failed");
 }
