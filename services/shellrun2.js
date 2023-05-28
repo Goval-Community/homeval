febd1f7537f11825031d11796325754b33529250
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2023-05-28 13:55:03 -0700

    Add WIP shellrun2 service

---
+++ services/shellrun2.js
@@ -0,0 +1,26 @@
+class Service extends ServiceBase {
+    constructor(...args) {
+        super(...args)
+        this.pty = new PtyProcess(this.id, process.env.SHELL || "sh")
+        this.pty.init().then(_ => {
+            console.debug("shell pty obtained:", this.pty.id)
+        })
+    }
+    
+	async recv(cmd, session) {
+		if (cmd.input) {
+			await this.pty.write(cmd.input)
+		}
+	}
+
+    async attach(session) {
+        await this.pty.add_session(session)
+    }
+}
+
+const service = new Service(
+	serviceInfo.id,
+	serviceInfo.service,
+	serviceInfo.name,
+);
+await service.start()
