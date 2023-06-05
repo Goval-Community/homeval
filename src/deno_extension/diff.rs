661d509319d9001f3dd82548fe62dedee163457c
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2023-06-04 17:37:56 -0700

    refactor: Run diff algo on blocking thread The diff can take up to 1 second, which isn't a lot but would still likely block other tasks, so it was moved to a thread where blocking is acceptable.

--- src/deno_extension/diff.rs
+++ src/deno_extension/diff.rs
@@ -13,53 +13,58 @@ enum DiffPart {
 
 #[op]
 async fn op_diff_texts(old: String, new: String) -> Result<Vec<DiffPart>, AnyError> {
-    let mut _differ = TextDiff::configure();
-    let differ = _differ.timeout(Duration::from_secs(1));
-    let diff = differ.diff_chars(&old, &new);
+    let parts = tokio::task::spawn_blocking(move || {
+        let mut _differ = TextDiff::configure();
+        let differ = _differ.timeout(Duration::from_secs(1));
+        let diff = differ.diff_chars(&old, &new);
 
-    let mut parts = vec![];
-    let mut last_op: Option<DiffPart> = None;
-    for part in diff.iter_all_changes() {
-        let mut new_op: Option<DiffPart> = None;
-        match part.tag() {
-            similar::ChangeTag::Equal => {
-                if let Some(DiffPart::Skip(amount)) = last_op.clone() {
-                    last_op = Some(DiffPart::Skip(amount + part.value().len() as u32))
-                } else {
-                    new_op = Some(DiffPart::Skip(part.value().len() as u32));
+        let mut parts = vec![];
+        let mut last_op: Option<DiffPart> = None;
+        for part in diff.iter_all_changes() {
+            let mut new_op: Option<DiffPart> = None;
+            match part.tag() {
+                similar::ChangeTag::Equal => {
+                    if let Some(DiffPart::Skip(amount)) = last_op.clone() {
+                        last_op = Some(DiffPart::Skip(amount + part.value().len() as u32))
+                    } else {
+                        new_op = Some(DiffPart::Skip(part.value().len() as u32));
+                    }
+                }
+                similar::ChangeTag::Delete => {
+                    if let Some(DiffPart::Delete(amount)) = last_op.clone() {
+                        last_op = Some(DiffPart::Delete(amount + part.value().len() as u32))
+                    } else {
+                        new_op = Some(DiffPart::Delete(part.value().len() as u32));
+                    }
+                }
+                similar::ChangeTag::Insert => {
+                    if let Some(DiffPart::Insert(same)) = last_op.clone() {
+                        last_op = Some(DiffPart::Insert(same + part.value()))
+                    } else {
+                        new_op = Some(DiffPart::Insert(part.value().to_string()));
+                    }
                 }
             }
-            similar::ChangeTag::Delete => {
-                if let Some(DiffPart::Delete(amount)) = last_op.clone() {
-                    last_op = Some(DiffPart::Delete(amount + part.value().len() as u32))
-                } else {
-                    new_op = Some(DiffPart::Delete(part.value().len() as u32));
+
+            if let Some(new_part) = new_op {
+                if let Some(last_part) = last_op.clone() {
+                    parts.push(last_part);
                 }
-            }
-            similar::ChangeTag::Insert => {
-                if let Some(DiffPart::Insert(same)) = last_op.clone() {
-                    last_op = Some(DiffPart::Insert(same + part.value()))
-                } else {
-                    new_op = Some(DiffPart::Insert(part.value().to_string()));
-                }
+
+                last_op = Some(new_part);
             }
         }
 
-        if let Some(new_part) = new_op {
-            if let Some(last_part) = last_op.clone() {
-                parts.push(last_part);
+        if let Some(op) = last_op {
+            match op {
+                DiffPart::Skip(_) => {}
+                _ => parts.push(op),
             }
-
-            last_op = Some(new_part);
         }
-    }
 
-    if let Some(op) = last_op {
-        match op {
-            DiffPart::Skip(_) => {}
-            _ => parts.push(op),
-        }
-    }
+        parts
+    })
+    .await?;
 
     Ok(parts)
 }
